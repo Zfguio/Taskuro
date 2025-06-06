9195333e9c8c8b731d9f382f7400664cb03bedaf
rvrct <rvrct@MAINPC>
2025-06-06 22:44:23 +0200

    Add project files.

---
+++ Taskuro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Taskuro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Taskuro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Taskuro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
