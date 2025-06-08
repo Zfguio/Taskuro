978edfccabd0454c2f068abb5996529257e26a2b
Kraus Michal <rvrctvyb@gmail.com>
2025-06-08 16:09:22 +0200

    after changing pages, the themes were loaded after first page was resulting in error "Microsoft.Maui.Controls.Xaml.XamlParseException: 'Position 11:11. StaticResource not found for key BackgroundColor'"

--- Taskuro/App.xaml.cs
+++ Taskuro/App.xaml.cs
@@ -1,18 +1,22 @@
-﻿using Taskuro.Views.Public.LoginPage;
-
+﻿
 namespace Taskuro
 {
     public partial class App : Application
     {
-        Page LoginPageView = new Views.Public.LoginPage.LoginPageView();    
+        Page HomePageView;
+
         public App()
         {
             InitializeComponent();
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(new Taskuro.Resources.Styles.Dark());// well this needs to be here, otherwise the styles are not applied correctly
+            // i will also treat dark mode as defult theme. unconventional, but i like it that way
+            HomePageView = new Views.Private.HomePage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new NavigationPage(LoginPageView));
+            return new Window(new NavigationPage(HomePageView));
         }
     }
 }
