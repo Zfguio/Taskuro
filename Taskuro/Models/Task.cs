2346f845f32bfb3bf17701863a9d5af1e43c174c
Kraus Michał <114016686+Zfguio@users.noreply.github.com>
2025-06-17 21:52:19 +0200

    Enhance Task model with nullable properties
    Updated the Task model to allow for nullable properties, including Title, Priority, CreatedAt, DueDate, Category, Tags, Notes, Status, and AssignedTo. Added IsCompleted property with a default value of false to track task completion status.

--- Taskuro/Models/Task.cs
+++ Taskuro/Models/Task.cs
@@ -6,8 +6,17 @@ namespace Taskuro.Models
     {
         [PrimaryKey,AutoIncrement]
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
+        public int? Priority { get; set; } 
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public string? Category { get; set; }
+        public string? Tags { get; set; } 
+        public string? Notes { get; set; } 
+        public string? Status { get; set; } 
+        public string? AssignedTo { get; set; } 
 
     }
 }
