c40f10ae080260db9c4d7a88c77d6997694c93fd
Kraus Michał <114016686+Zfguio@users.noreply.github.com>
2025-06-17 21:58:26 +0200

    Refactor database handling and clean up project files
    Updated `Database.cs` to use the new `Tasks` model and improved asynchronous method practices. Added methods for adding, retrieving, and updating tasks.
    
    Removed unnecessary entries related to the `Servicies` directory in `Taskuro.csproj` and added a reference to `Microsoft.EntityFrameworkCore.Tools` for enhanced database operations.

--- Taskuro/Data/Database.cs
+++ Taskuro/Data/Database.cs
@@ -1,18 +1,39 @@
 ﻿using SQLite;
+using Taskuro.Models;
 namespace Taskuro.Data
 {
     public class Database
     {
         static string DataBasePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Taskuro.db";
         static SQLiteAsyncConnection db;
-        static async void InitializeDatabase()
+        static async Task InitializeDatabase()
         {
             if(db != null)
                 return;
             else
                db = new SQLiteAsyncConnection(DataBasePath);
 
-            await db.CreateTableAsync<Models.Task>();
+            await db.CreateTableAsync<Tasks>();
+        }
+        static public async Task addTask(Tasks task)
+        {
+            await InitializeDatabase();
+            await db.InsertAsync(task);
+        }
+        static public async Task<List<Tasks>> getTasksAsync()
+        {
+            await InitializeDatabase();
+            return await db.Table<Tasks>().ToListAsync();
+        }
+        static public async Task<Tasks> getTaskByIDAsync(int id)
+        {
+            await InitializeDatabase();
+            return await db.Table<Tasks>().Where(t => t.Id == id).FirstOrDefaultAsync();
+        }
+        static public async Task updateTask(Tasks task)
+        {
+            await InitializeDatabase();
+            await db.UpdateAsync(task);
         }
     }
 }
